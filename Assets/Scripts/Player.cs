2017106b06d42a709df032a669a7ecbdf81d7f40
Kian Agahi <107733623+Konic004@users.noreply.github.com>
2024-10-01 16:21:59 -0700

    projectile system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private GameObject bullet;
+    private float gunHeat;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,15 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject obj = Instantiate(bullet, new Vector3(transform.position.x + 0.85f, transform.position.y + 1, 0), transform.rotation);
+        obj.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 10);
     }
 }
