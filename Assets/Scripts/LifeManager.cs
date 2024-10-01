e413cc64d498513046aa39bf994cb4230e6c2221
Jason1add <1833029317@qq.com>
2024-09-30 23:48:00 -0700

    1
    1

--- Assets/Scripts/LifeManager.cs
+++ Assets/Scripts/LifeManager.cs
@@ -1,21 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro;  
 using UnityEngine.SceneManagement;
 
-public class PlayerController : MonoBehaviour
+public class LifeManager : MonoBehaviour
 {
+    public int lives = 3;  
+    public TextMeshProUGUI livesText; 
+    
     public GameObject gameOverScene; 
     public GameObject startGameText; 
 
     private bool gameIsOver = false; 
     private bool gameIsStarted = false; 
 
+
     void Start()
     {
-        
+        UpdateLivesText();
         Time.timeScale = 0;
         startGameText.SetActive(true);
-        gameOverScene.SetActive(false); 
+        gameOverScene.SetActive(false);
+    }
+
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+{
+    // 检查进入 GameOverZone 的物体是否具有 "Enemy" 标签
+    if (collision.CompareTag("Enemy"))
+    {
+        Debug.Log("Enemy hit the GameOverZone");
+        
+        // 生命值减少
+        lives--;
+        UpdateLivesText();
+
+        // 如果生命值小于或等于0，执行游戏结束逻辑
+        if (lives <= 0)
+        {
+            GameOver();
+        }
+    }
+}
+
+
+
+    
+    void UpdateLivesText()
+    {
+        livesText.text = "Lives: " + lives;
     }
 
     void Update()
@@ -56,14 +90,6 @@ public class PlayerController : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
     }
 
-   
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-       
-
-        if (collision.gameObject.tag == "GameOverZone")
-        {
-            GameOver(); 
-        }
-    }
+    
 }
+
