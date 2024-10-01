620f3116dc131f82de91d6e3ebb8cb4829548a4f
Kian Agahi <107733623+Konic004@users.noreply.github.com>
2024-09-30 21:24:19 -0700

    enemy spawner and AI

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(player, new Vector3(0, -3, 0), transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject enemy;
+    public float minX, maxX, minY, maxY, timeBetweenSpawn;
+    private float spawnTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > spawnTime)
+        {
+            SpawnEnemy(enemy);
+            spawnTime = Time.time + timeBetweenSpawn;
+        }
+    }
+
+
+    void SpawnEnemy(GameObject enemy)
+    {
+        float randomX = Random.Range(minX, maxX);
+        float randomY = Random.Range(minY, maxY);
+        Instantiate(enemy, transform.position + new Vector3(randomX, randomY, 0), transform.rotation);
+    }
+}
