f9694f2c5d1e636a40cf67358616a1a110d95f62
Kian Agahi <107733623+Konic004@users.noreply.github.com>
2024-10-01 17:13:54 -0700

    improvement to projectile system

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -12,15 +12,24 @@ public class EnemyAI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         //distance = Vector2.Distance(transform.position, player.transform.position);
-       // Vector2 direction = player.transform.position - transform.position;
+        //Vector2 direction = player.transform.position - transform.position;
 
         transform.position = Vector2.MoveTowards(this.transform.position, new Vector2(this.transform.position.x, -10), speed * Time.deltaTime);
+
+    }
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("COLLISION!");
+        Destroy(this.gameObject);
+        Destroy(collision.gameObject);
     }
 }
